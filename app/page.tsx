1979ee79c83be61915673bd1763b5e85e7c9653b
safi-siddiqui-github <safisiddiqui.work@gmail.com>
2025-12-04 02:18:49 +0500

    wip

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex flex-col">
-      <h2 className="text-xl">Home Page</h2>
-      <p className="">Safi Siddiqui Next.js App</p>
-    </div>
-  );
-}
